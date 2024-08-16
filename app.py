346904439622b7e111f5759b3a93c08acb6bcf39
Ishaan Roy <ishaanroy@Ishaans-MacBook-Air.local>
2024-08-16 13:28:51 -0600

    Modified main coroutine

--- app.py
+++ app.py
@@ -3,6 +3,8 @@
 import asyncio
 import json
 import secrets
+import os 
+import signal
 
 import websockets
 
@@ -182,8 +184,14 @@ async def handler(websocket):
 
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
 
 
 if __name__ == "__main__":
